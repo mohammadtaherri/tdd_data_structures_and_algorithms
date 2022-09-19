d7ea51508c86aab7e9a19dc42cd43bc9b65fa758
Mohammad Taheri <taherimohammad08@gmail.com>
2022-09-19 22:09:28 +0430

    first commit

---
+++ bin/tdd_data_structures_and_algorithms.dart
@@ -0,0 +1,5 @@
+import 'package:tdd_data_structures_and_algorithms/tdd_data_structures_and_algorithms.dart' as tdd_data_structures_and_algorithms;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tdd_data_structures_and_algorithms.calculate()}!');
+}
---
+++ test/tdd_data_structures_and_algorithms_test.dart
@@ -0,0 +1,8 @@
+import 'package:tdd_data_structures_and_algorithms/tdd_data_structures_and_algorithms.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
