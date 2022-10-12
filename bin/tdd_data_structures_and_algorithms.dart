94d75b9e1633e7b50df99861ee7bdac12467b499
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-12 20:46:38 +0330

    clean project

--- bin/tdd_data_structures_and_algorithms.dart
+++ bin/tdd_data_structures_and_algorithms.dart
@@ -1,5 +1,5 @@
 import 'package:tdd_data_structures_and_algorithms/tdd_data_structures_and_algorithms.dart' as tdd_data_structures_and_algorithms;
 
-void main(List<String> arguments) {
-  print('Hello world: ${tdd_data_structures_and_algorithms.calculate()}!');
+void main() {
+  
 }
--- test/tdd_data_structures_and_algorithms_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:tdd_data_structures_and_algorithms/tdd_data_structures_and_algorithms.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
