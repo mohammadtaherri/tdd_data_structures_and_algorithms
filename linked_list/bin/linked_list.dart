206a0f08a53a85ad7e70d717cbc490a6e839d99e
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-17 18:42:14 +0330

    create new project named linked_list

---
+++ linked_list/bin/linked_list.dart
@@ -0,0 +1,5 @@
+import 'package:linked_list/linked_list.dart' as linked_list;
+
+void main(List<String> arguments) {
+  
+}
---
+++ linked_list/test/linked_list_test.dart
@@ -0,0 +1,6 @@
+import 'package:linked_list/linked_list.dart';
+import 'package:test/test.dart';
+
+void main() {
+  
+}
