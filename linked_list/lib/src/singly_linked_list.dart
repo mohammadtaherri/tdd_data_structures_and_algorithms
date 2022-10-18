cf4f20731e663aecf835e3d0f49641b793fc1575
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-18 18:04:32 +0330

    refactor(linked_list): some refactoring

--- linked_list/lib/src/singly_linked_list.dart
+++ linked_list/lib/src/singly_linked_list.dart
@@ -1,13 +1,12 @@
 part of linked_list;
 
 class SinglyLinkedList<E extends LinkedListEntry>{
-  get isEmpty => _size == 0;
 
-  get size => _size;
+  bool get isEmpty => _size == 0;
+  int get size => _size;
   int _size = 0;
 
   get first => null;
-
   get last => null;
 
   void addFirst(E entry) {
