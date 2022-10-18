62b82c95188e864e2be8e1caab37e0f0c41301a6
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-18 17:03:21 +0330

    test(linked_list): test newly created list should be empty

--- linked_list/test/src/singly_linked_list_test.dart
+++ linked_list/test/src/singly_linked_list_test.dart
@@ -6,16 +6,37 @@ void main() {
   runTest(SinglyLinkedListTest());
 }
 
-class SinglyLinkedListTest extends RootTestGroup{
+class SinglyLinkedListTest extends RootTestGroup {
   SinglyLinkedListTest()
-      : super(
-        groups: [
-
-        ]
-      );
+      : super(groups: [
+          GivenNewlyCreatedList(
+            groups: [],
+          )
+        ]);
 }
 
 class GivenNewlyCreatedList extends BranchTestGroup{
   GivenNewlyCreatedList({required super.groups})
       : super(groupDescription: 'given newly created list');
+
+  void shouldBeEmpty(){
+    SinglyLinkedList linkedList = SinglyLinkedList();
+    expect(linkedList.isEmpty, isTrue);
+    expect(linkedList.size, isZero);
+  }
+
+  void firstAndLastShouldBeNull(){
+    SinglyLinkedList linkedList = SinglyLinkedList();
+    expect(linkedList.first, isNull);
+    expect(linkedList.last, isNull);
+  }
+
+  @override
+  void registerTests(TestContainer container) {
+    container['should be empty'] = 
+        Test(shouldBeEmpty);
+
+    container['first and last should be null'] = 
+        Test(firstAndLastShouldBeNull);
+  }
 }
