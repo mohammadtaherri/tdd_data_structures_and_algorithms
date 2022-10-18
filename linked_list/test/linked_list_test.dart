89c3b5469bb97cea0c16466b5977a59906573b22
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-18 11:31:58 +0330

    test(linked_list): create SinglyLinkedListTest class and run it in main

--- linked_list/test/linked_list_test.dart
+++
@@ -1,6 +0,0 @@
-import 'package:linked_list/linked_list.dart';
-import 'package:test/test.dart';
-
-void main() {
-  
-}
---
+++ linked_list/test/src/singly_linked_list_test.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:test/test.dart';
+import 'package:clean_test/clean_test.dart';
+import 'package:linked_list/linked_list.dart';
+
+void main() {
+  runTest(SinglyLinkedListTest());
+}
+
+class SinglyLinkedListTest extends RootTestGroup {
+  SinglyLinkedListTest()
+      : super(
+          groups: [
+          ],
+        );
+}
+
