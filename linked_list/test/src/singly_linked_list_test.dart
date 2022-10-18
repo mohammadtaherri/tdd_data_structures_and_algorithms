3ac3101837405a3a1420fb26ba2952ff3ecd3eec
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-18 16:53:06 +0330

    test(linked_list): create SinglyLinkedListTest class

--- linked_list/test/src/singly_linked_list_test.dart
+++ linked_list/test/src/singly_linked_list_test.dart
@@ -3,7 +3,14 @@ import 'package:clean_test/clean_test.dart';
 import 'package:linked_list/linked_list.dart';
 
 void main() {
-  test('singly linked list ...', () async {
-    // TODO: Implement test
-  });
+  runTest(SinglyLinkedListTest());
+}
+
+class SinglyLinkedListTest extends RootTestGroup{
+  SinglyLinkedListTest()
+      : super(
+        groups: [
+
+        ]
+      );
 }
