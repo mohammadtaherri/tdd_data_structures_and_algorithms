9bfb641f3eb96bf97b365817ba73af8f4f7eecd2
Mohammad Taheri <taherimohammad08@gmail.com>
2022-10-18 16:50:07 +0330

    test(linked_list): create singly_linked_list_test.dart

---
+++ linked_list/test/src/singly_linked_list_test.dart
@@ -0,0 +1,9 @@
+import 'package:test/test.dart';
+import 'package:clean_test/clean_test.dart';
+import 'package:linked_list/linked_list.dart';
+
+void main() {
+  test('singly linked list ...', () async {
+    // TODO: Implement test
+  });
+}
