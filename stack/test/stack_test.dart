be4b9f4acc8bea53e5001848f551a50aeb0f465b
mohammadtaherri <taherimohammad08@gmail.com>
2022-10-28 19:20:30 +0330

    refactor(stack): refactor tests to use annotations

--- stack/test/stack_test.dart
+++ stack/test/stack_test.dart
@@ -1,191 +1,15 @@
 // ignore_for_file: non_constant_identifier_names
+library stack_test;
+
 import 'package:test/test.dart';
-import 'package:clean_test/clean_test.dart';
+import 'package:clean_test/annotated_test.dart';
 import 'package:stack/stack.dart';
 
 void main() {
-  runTest(StackTest());
+  runTests(libraryName: 'stack_test');
 }
 
-class StackTest extends RootTestGroup{
-  StackTest()
-    : super(
-      groups: [
-        GivenNwelyCreatedStackWithPositiveCapacity(),
-        NegativeCapacityStack(),
-        GivenZeroCapacityStack(),
-      ],
-    );
-}
-
-class GivenNwelyCreatedStackWithPositiveCapacity extends LeafTestGroup with ComposedExpect{
-
-  @override
-  late Stack<int> stack;
-
-  @override
-  void setUp() {
-    stack = Stack<int>(capacity: 3);
-  }
-
-  void shouldBeEmpty(){
-    expectStackIsEmpty();
-  }
-
-  void push_ShouldIncrementSizeByOne(){
-    stack.push(10);
-    expectStackSizeIsOne();
-
-    stack.push(10);
-    expectStackSizeIsTwo();
-  }
-
-  void push_WhenPastCapacity_ShouldThrowStackOverFlow(){
-    void act(){
-      stack.push(10);
-      stack.push(10);
-      stack.push(10);
-      stack.push(10);
-    }
-
-    expect(act, throwsA(isA<StackOverflowException>()));
-  }
-
-  void pop_ShouldThrowStackUnderFLow(){
-    void act(){
-      stack.pop();
-    }
-
-    expect(act, throwsA(isA<StackUnderflowException>()));
-  }
-
-  void pop_GivenPushingTwoItems_ShouldDecrementSizeByOne(){
-    stack.push(10);
-    stack.push(10);
-
-    stack.pop();
-    expectStackSizeIsOne();
-
-    stack.pop();
-    expectStackIsEmpty();
-  }
-
-  void peek_ShouldThrowStackEmpty(){
-    void act(){
-      stack.peek();
-    }
-
-    expect(act, throwsA(isA<StackEmptyException>()));
-  }
-
-  void push_pop_WhenXIsPushed_XShouldBePopped(){
-    final x = 10;
-    stack.push(x);
-    expect(stack.pop(), equals(x));
-  }
-
-  void push_pop_WhenXAndYArePushed_YAndXShouldBePopped(){
-    final x = 10;
-    final y = 20;
-    stack.push(x);
-    stack.push(y);
-    expect(stack.pop(), equals(y));
-    expect(stack.pop(), equals(x));
-  }
-
-  void peek_GivenPushingXAndY_ShouldReturnY(){
-    final x = 10;
-    final y = 20;
-    stack.push(x);
-    stack.push(y);
-    expect(stack.peek(), equals(y));
-  }
-
-  @override
-  void registerTests(TestContainer container) {
-    container.addAll([
-      Test(shouldBeEmpty),
-      Test(push_ShouldIncrementSizeByOne),
-      Test(push_WhenPastCapacity_ShouldThrowStackOverFlow),
-      Test(pop_ShouldThrowStackUnderFLow),
-      Test(pop_GivenPushingTwoItems_ShouldDecrementSizeByOne),
-      Test(peek_ShouldThrowStackEmpty),
-      Test(push_pop_WhenXIsPushed_XShouldBePopped),
-      Test(push_pop_WhenXAndYArePushed_YAndXShouldBePopped),
-      Test(peek_GivenPushingXAndY_ShouldReturnY),
-    ]);
-  }
-}
-
-class NegativeCapacityStack extends LeafTestGroup{
-
-  void whenStackWithNegativeCapacityIsCreated_IllegalCapacityShouldBeThrown(){
-    void act(){
-      Stack<int>(capacity: -1);
-    }
-
-    expect(act, throwsA(isA<IllegalCapacityException>()));
-  }
-
-  @override
-  void registerTests(TestContainer container) {
-    container.addAll([
-      Test(whenStackWithNegativeCapacityIsCreated_IllegalCapacityShouldBeThrown),
-    ]);
-  }
-}
-
-
-class GivenZeroCapacityStack extends LeafTestGroup with ComposedExpect{
-
-  @override
-  late Stack<int> stack;
-
-  @override
-  void setUp() {
-    stack = Stack<int>(capacity: 0);
-  }
-
-  void shouldBeEmpty(){
-    expectStackIsEmpty();
-  }
-
-  void push_ShouldThrowStackOverFlow(){
-    void act(){
-      stack.push(10);
-    }
-
-    expect(act, throwsA(isA<StackOverflowException>()));
-  }
-
-  void pop_ShouldThrowStackUnderFLow(){
-    void act(){
-      stack.pop();
-    }
-
-    expect(act, throwsA(isA<StackUnderflowException>()));
-  }
-
-  void peek_ShouldThrowStackEmpty(){
-    void act(){
-      stack.peek();
-    }
-
-    expect(act, throwsA(isA<StackEmptyException>()));
-  }
-
-  @override
-  void registerTests(TestContainer container) {
-    container.addAll([
-      Test(shouldBeEmpty),
-      Test(push_ShouldThrowStackOverFlow),
-      Test(pop_ShouldThrowStackUnderFLow),
-      Test(peek_ShouldThrowStackEmpty),
-    ]);
-  }
-}
-
-mixin ComposedExpect on TestGroup{
+mixin ComposedExpect{
 
   Stack get stack;
 
@@ -204,3 +28,158 @@ mixin ComposedExpect on TestGroup{
     expect(stack.isEmpty, isFalse);
   }
 }
+
+
+
+@TestCase()
+@Root()
+class StackTest with ComposedExpect{
+  @override
+  late final Stack stack;
+}
+
+@TestCase()
+class GivenNewlyCreatedStackWith3Capacity extends StackTest{
+
+  @SetUp()
+  void createStackWith3Capacity() {
+    stack = Stack<int>(capacity: 3);
+  }
+
+  @Test()
+  void shouldBeEmpty(){
+    expectStackIsEmpty();
+  }
+
+  @Test()
+  void push_ShouldIncrementSizeByOne(){
+    stack.push(10);
+    expectStackSizeIsOne();
+
+    stack.push(10);
+    expectStackSizeIsTwo();
+  }
+
+  @Test()
+  void push_WhenPastCapacity_ShouldThrowStackOverFlow(){
+    void act(){
+      stack.push(10);
+      stack.push(10);
+      stack.push(10);
+      stack.push(10);
+    }
+
+    expect(act, throwsA(isA<StackOverflowException>()));
+  }
+
+  @Test()
+  void pop_ShouldThrowStackUnderFLow(){
+    void act(){
+      stack.pop();
+    }
+
+    expect(act, throwsA(isA<StackUnderflowException>()));
+  }
+
+  @Test()
+  void pop_GivenPushingTwoItems_ShouldDecrementSizeByOne(){
+    stack.push(10);
+    stack.push(10);
+
+    stack.pop();
+    expectStackSizeIsOne();
+
+    stack.pop();
+    expectStackIsEmpty();
+  }
+
+  @Test()
+  void peek_ShouldThrowStackEmpty(){
+    void act(){
+      stack.peek();
+    }
+
+    expect(act, throwsA(isA<StackEmptyException>()));
+  }
+
+  @Test()
+  void push_pop_WhenXIsPushed_XShouldBePopped(){
+    final x = 10;
+    stack.push(x);
+    expect(stack.pop(), equals(x));
+  }
+
+  @Test()
+  void push_pop_WhenXAndYArePushed_YAndXShouldBePopped(){
+    final x = 10;
+    final y = 20;
+    stack.push(x);
+    stack.push(y);
+    expect(stack.pop(), equals(y));
+    expect(stack.pop(), equals(x));
+  }
+
+  @Test()
+  void peek_GivenPushingXAndY_ShouldReturnY(){
+    final x = 10;
+    final y = 20;
+    stack.push(x);
+    stack.push(y);
+    expect(stack.peek(), equals(y));
+  }
+}
+
+@TestCase()
+class NegativeCapacityStack extends StackTest{
+
+  @Test()
+  void whenStackWithNegativeCapacityIsCreated_IllegalCapacityShouldBeThrown(){
+    void act(){
+      Stack<int>(capacity: -1);
+    }
+
+    expect(act, throwsA(isA<IllegalCapacityException>()));
+  }
+}
+
+@TestCase()
+class GivenZeroCapacityStack extends StackTest{
+
+  @SetUp()
+  void createStackWithZeroCapacity() {
+    stack = Stack<int>(capacity: 0);
+  }
+
+  @Test()
+  void shouldBeEmpty(){
+    expectStackIsEmpty();
+  }
+
+  @Test()
+  void push_ShouldThrowStackOverFlow(){
+    void act(){
+      stack.push(10);
+    }
+
+    expect(act, throwsA(isA<StackOverflowException>()));
+  }
+
+  @Test()
+  void pop_ShouldThrowStackUnderFLow(){
+    void act(){
+      stack.pop();
+    }
+
+    expect(act, throwsA(isA<StackUnderflowException>()));
+  }
+
+  @Test()
+  void peek_ShouldThrowStackEmpty(){
+    void act(){
+      stack.peek();
+    }
+
+    expect(act, throwsA(isA<StackEmptyException>()));
+  }
+}
+
