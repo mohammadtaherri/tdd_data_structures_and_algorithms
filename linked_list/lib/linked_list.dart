8a49bb7bee31afdf7b4cabbd897556cda24f8b2d
mohammadtaherri <taherimohammad08@gmail.com>
2022-10-31 12:01:00 +0330

    feat(linked_list): create abstract class LinkedList

--- linked_list/lib/linked_list.dart
+++ linked_list/lib/linked_list.dart
@@ -2,5 +2,30 @@ library linked_list;
 
 part './src/singly_linked_list_old.dart';
 part './src/singly_linked_list.dart';
+part './src/circular_linked_list.dart';
 part './src/errors.dart';
+
+
+abstract class LinkedList<E extends LinkedListEntry<E>>{
+
+  bool get isEmpty;
+  int get size;
+
+  E? get first;
+  E? get last;
+
+  void addFirst(E entry);
+  void addLast(E entry);
+  void removeFirst();
+  void removeLast();
+
+  E elementAt(int index);
+  int indexOf(E entry);
+  
+  void reverse();
+}
+
+abstract class LinkedListEntry<E extends LinkedListEntry<E>>{
+  E? next;
+}
 
