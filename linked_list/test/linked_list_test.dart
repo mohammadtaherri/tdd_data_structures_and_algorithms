edb956f2cc889cdb6009917b2e76789cf5c0e0fc
mohammadtaherri <taherimohammad08@gmail.com>
2022-10-31 11:36:07 +0330

    test(linked_list): run CircularLinkedListTest

---
+++ linked_list/test/linked_list_test.dart
@@ -0,0 +1,9 @@
+
+import 'package:clean_test/annotated_test.dart';
+import 'src/singly_linked_list_old_test.dart';
+
+
+void main() {
+  // runTestsByLibraryPath('src/singly_linked_list_test.dart');
+  runTestsByLibraryPath('src/circular_linked_list_test.dart');
+}
