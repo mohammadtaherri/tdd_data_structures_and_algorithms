b464ecb098e2a2b7fe7863b847a8f3803bdb94c0
mohammadtaherri <taherimohammad08@gmail.com>
2022-11-05 10:05:44 +0330

    test(queue): run ArrayQueueTest

--- queue/test/queue_test.dart
+++ queue/test/queue_test.dart
@@ -1,4 +1,7 @@
-import 'package:queue/queue.dart';
-import 'package:test/test.dart';
+import 'package:dartunit/dartunit.dart';
+
+void main() {
+  runTestsByLibraryPath('src/array_queue_test.dart');
+}
 
 
