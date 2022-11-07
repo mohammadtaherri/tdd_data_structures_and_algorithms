fa2cc3826bd22937bace589e1eba4b25956127cc
mohammadtaherri <taherimohammad08@gmail.com>
2022-11-07 14:29:58 +0330

    refactor(queue): extend QueueBase instead of Queue

--- queue/lib/src/priority_queue.dart
+++ queue/lib/src/priority_queue.dart
@@ -1,42 +1,22 @@
-import 'package:queue/queue.dart';
-
 import 'queue_base.dart';
 
-class PriorityQueue<T extends PriorityQueueItem<T>> extends Queue<T>{
+class PriorityQueue<T extends PriorityQueueItem<T>> extends QueueBase<T>{
 
-  factory PriorityQueue({int capacity = 5}){
-    if(capacity < 0)
-      throw IllegalCapcityException();
+  factory PriorityQueue({int capacity = 5}) => QueueBase<T>.make(
+        capacity: capacity,
+        maker: () => capacity == 0
+            ? _ZeroCapacityPriorityQueue()
+            : PriorityQueue._(capacity: capacity),
+      ) as PriorityQueue<T>;
 
-    if(capacity == 0)
-      return _ZeroCapacityPriorityQueue();
+  PriorityQueue._({super.capacity})
+      : _items = List.filled(capacity, null);
 
-    return PriorityQueue._(capacity: capacity);
-  }
-
-  PriorityQueue._({int capacity = 5})
-      : _capacity = capacity,
-        _items = List.filled(capacity, null);
-
-  final int _capacity;
   final List<T?> _items;
 
   @override
-  bool get isEmpty => _size == 0;
-
-  @override
-  bool get isFull => _size == _capacity;
-
-  @override
-  int get size => _size;
-  int _size = 0;
-
-  @override
-  void enqueue(T item) {
-    if(isFull)
-      throw FullQueueException();
-
-    int lastIndex = _size - 1;
+  void internalEnqueue(T item) {
+    int lastIndex = size - 1;
 
     while(lastIndex > -1){
       T current = _items[lastIndex]!;
@@ -49,23 +29,16 @@ class PriorityQueue<T extends PriorityQueueItem<T>> extends Queue<T>{
     }
 
     _items[lastIndex + 1] = item;
-    _size++;
   }
 
   @override
-  T dequeue() {
-    if(isEmpty)
-      throw EmptyQueueException();
-
-    return _items[--_size]!;
+  T internalDequeue() {
+    return _items[size]!;
   }
 
   @override
-  T peek() {
-    if(isEmpty)
-      throw EmptyQueueException();
-
-    return _items[_size - 1]!;
+  T internalPeek() {
+    return _items[size - 1]!;
   } 
 }
 
